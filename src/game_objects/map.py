5f000f40b37455e792e7b060760f735cd0271be3
IvelinIskrenov <ivelin_iskrenov@abv.bg>
2023-08-18 17:10:24 +0300

    Added valid map check.

--- src/game_objects/map.py
+++ src/game_objects/map.py
@@ -29,5 +29,62 @@ class Map(Object):
         self.columns: int = columns
         self.tiles: list[list[Tile]] = ([Tile()]*columns)*rows
 
+    def _dfs_check( self,
+                    i: int,
+                    j: int,
+                    visited: list[list[bool]]) -> str:
+        """
+        Given tile coordinates on the map:
+        Checks if the map is valid with depth first search.
+        Returns "valid" if yes.
+        if no - returns:
+        "not_SCC" if the component is not strongly connected,
+        since the snake won't be able to go both ways.
+        "out_of_bounds" if the snake can get out of the map.
+        """
+        if i<0 or i>=self.rows or j<0 or j>=self.columns:
+            return "out_of_bounds"
+        if visited[i][j]:
+            return "valid"
+        if self.tiles[i][j].is_snake_obstruction():
+            visited[i][j] = True
+            return "valid"
+        
+        directions = [1, -1]
 
+        for direction in directions:
+            result = self._dfs_check(i + direction)
+            if result != "valid":
+                return result
+            result2 = self._dfs_check(j + direction)
+            if result != "valid":
+                return result
+
+        for direction_x in directions:
+            for direction_y in directions:
+                i_new = i + direction_x
+                j_new = j + direction_y
+                if not\
+                    (self.tiles[i_new][j_new].is_snake_obstruction()\
+                    or self.tiles[i][j_new].is_snake_obstruction()\
+                    or self.tiles[i_new][j_new].is_snake_obstruction()):
+                    return "valid"
+        return "non_SCC"
+    
+    def check_map_valid(self) -> str:
+        SCC_count = 0
+        visited = ([False]*self.columns)*self.rows
+        for i in range(self.rows):
+            for j in range(self.columns):
+                if not visited[i][j]\
+                and not self.tiles[i][j].is_snake_obstruction():
+                    SCC_count += 1
+                    result = self._dfs_check(i, j, visited)
+                    if result != "valid":
+                        return result
+        if SCC_count < 0:
+            return "no_space"
+        if SCC_count > 1:
+            return "multiple_SCC"
+        return "valid"
 
